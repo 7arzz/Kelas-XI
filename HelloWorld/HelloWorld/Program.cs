511df92684f72aa9e8aa9bf6e4bdec6b37f61caa
7arzz <7arzz404@gmail.com>
2026-07-15 09:28:54 +0700

    initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BelajarCSharp // Biasanya sesuai nama project, tapi bisa di ubah sesuai kebutuhan
+{
+    class program // Class program sebagai entry point aplikasi (tempat nulis kodingan)
+    {
+        static void Main(string[] args) //main adalah method utama yang akan dijalankan pertama kali saat program di jalankan
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+
+}
