d0d6d3fdb64826156a5a08399cd6268b838697f1
7arzz <7arzz404@gmail.com>
2026-07-22 10:03:43 +0700

    calculator zodiak

--- 22-07-26/CalculatorConsole/CalculatorConsole/Program.cs
+++ 22-07-26/CalculatorConsole/CalculatorConsole/Program.cs
@@ -9,6 +9,7 @@ namespace CalculatorConsole
             CalculatorSederhana sederhana = new CalculatorSederhana();
             CalculatorDatar datar = new CalculatorDatar();
             CalculatorRuang ruang = new CalculatorRuang();
+            CalculatorZodiak zodiak = new CalculatorZodiak();
 
             bool jalan = true;
 
@@ -21,7 +22,8 @@ namespace CalculatorConsole
                 Console.WriteLine("1. Calculator Sederhana");
                 Console.WriteLine("2. Calculator Bangun Datar");
                 Console.WriteLine("3. Calculator Bangun Ruang");
-                Console.WriteLine("4. Keluar");
+                Console.WriteLine("4. Cari Zodiak");
+                Console.WriteLine("5. Keluar");
                 Console.WriteLine("====================================");
                 Console.Write("Pilih Menu : ");
 
@@ -44,6 +46,10 @@ namespace CalculatorConsole
                         break;
 
                     case 4:
+                        zodiak.Menu();
+                        break;
+
+                    case 5:
                         jalan = false;
                         Console.WriteLine("Terima kasih telah menggunakan Calculator Console.");
                         break;
