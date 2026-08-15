ddadf4b90c6322e1ca8f266bc9f64b1b1d46670e
7arzz <7arzz404@gmail.com>
2026-08-15 16:06:46 +0700

    SSMS dan EF done

---
+++ LKS/BelajarEF/BelajarEF/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace BelajarEF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.comboBoxJurusan = new System.Windows.Forms.ComboBox();
+            this.numericUpDownUmur = new System.Windows.Forms.NumericUpDown();
+            this.richTextBoxAlamat = new System.Windows.Forms.RichTextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nama = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Umur = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Alamat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Jurusan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.update = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownUmur)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Siswa";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Nama,
+            this.Umur,
+            this.Alamat,
+            this.Jurusan,
+            this.delete,
+            this.update});
+            this.dataGridView1.Location = new System.Drawing.Point(5, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(513, 410);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(550, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(550, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Umur";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(550, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Alamat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(546, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Jurusan";
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(623, 93);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(191, 20);
+            this.textBoxNama.TabIndex = 2;
+            // 
+            // comboBoxJurusan
+            // 
+            this.comboBoxJurusan.FormattingEnabled = true;
+            this.comboBoxJurusan.Location = new System.Drawing.Point(623, 185);
+            this.comboBoxJurusan.Name = "comboBoxJurusan";
+            this.comboBoxJurusan.Size = new System.Drawing.Size(191, 21);
+            this.comboBoxJurusan.TabIndex = 3;
+            // 
+            // numericUpDownUmur
+            // 
+            this.numericUpDownUmur.Location = new System.Drawing.Point(623, 136);
+            this.numericUpDownUmur.Name = "numericUpDownUmur";
+            this.numericUpDownUmur.Size = new System.Drawing.Size(191, 20);
+            this.numericUpDownUmur.TabIndex = 4;
+            // 
+            // richTextBoxAlamat
+            // 
+            this.richTextBoxAlamat.Location = new System.Drawing.Point(623, 235);
+            this.richTextBoxAlamat.Name = "richTextBoxAlamat";
+            this.richTextBoxAlamat.Size = new System.Drawing.Size(191, 96);
+            this.richTextBoxAlamat.TabIndex = 5;
+            this.richTextBoxAlamat.Text = "";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(697, 356);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(76, 39);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(588, 356);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(76, 39);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            // 
+            // Nama
+            // 
+            this.Nama.DataPropertyName = "Nama";
+            this.Nama.HeaderText = "Nama";
+            this.Nama.Name = "Nama";
+            this.Nama.Width = 135;
+            // 
+            // Umur
+            // 
+            this.Umur.DataPropertyName = "Umur";
+            this.Umur.HeaderText = "Umur";
+            this.Umur.Name = "Umur";
+            // 
+            // Alamat
+            // 
+            this.Alamat.DataPropertyName = "Alamat";
+            this.Alamat.HeaderText = "Alamat";
+            this.Alamat.Name = "Alamat";
+            // 
+            // Jurusan
+            // 
+            this.Jurusan.DataPropertyName = "Jurusan";
+            this.Jurusan.HeaderText = "Jurusan";
+            this.Jurusan.Name = "Jurusan";
+            this.Jurusan.Width = 135;
+            // 
+            // delete
+            // 
+            this.delete.HeaderText = "Delete";
+            this.delete.Name = "delete";
+            // 
+            // update
+            // 
+            this.update.HeaderText = "Update";
+            this.update.Name = "update";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Filter";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(75, 47);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(191, 20);
+            this.textBoxSearch.TabIndex = 2;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(272, 43);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(102, 30);
+            this.buttonSearch.TabIndex = 6;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 500);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.richTextBoxAlamat);
+            this.Controls.Add(this.numericUpDownUmur);
+            this.Controls.Add(this.comboBoxJurusan);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.textBoxNama);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownUmur)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.ComboBox comboBoxJurusan;
+        private System.Windows.Forms.NumericUpDown numericUpDownUmur;
+        private System.Windows.Forms.RichTextBox richTextBoxAlamat;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nama;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Umur;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Alamat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Jurusan;
+        private System.Windows.Forms.DataGridViewButtonColumn delete;
+        private System.Windows.Forms.DataGridViewButtonColumn update;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearch;
+    }
+}
+
---
+++ LKS/BelajarEF/BelajarEF/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BelajarEF
+{
+    public partial class Form1 : Form
+    {
+        SekolahDBEntities db = new SekolahDBEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int idSiswa;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            loadData();
+            
+
+            comboBoxJurusan.DataSource = db.Jurusans.ToList();
+            comboBoxJurusan.DisplayMember = "namaJurusan";
+            comboBoxJurusan.ValueMember = "Id";
+            comboBoxJurusan.SelectedIndex = -1;
+            
+        }
+
+        private void loadData()
+        {
+            dataGridView1.DataSource = db.Siswas.Select(s => new
+            {
+                s.Id,
+                s.Nama,
+                s.Umur,
+                s.Alamat,
+                Jurusan = s.Jurusan.namaJurusan
+            })
+                .ToList();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+            if (textBoxNama.Text.Trim() == "" || numericUpDownUmur.Value <= 0 || richTextBoxAlamat.Text.Trim() == "" || comboBoxJurusan.SelectedIndex == -1 )
+            {
+                MessageBox.Show("semua data harus di isi");
+                return;
+            }
+            
+                var siswa = new Siswa();
+
+                siswa.Nama = textBoxNama.Text;
+                siswa.Umur = Convert.ToInt32(numericUpDownUmur.Value);
+                siswa.Alamat = richTextBoxAlamat.Text;
+                siswa.IDJurusan = Convert.ToInt32(comboBoxJurusan.SelectedValue);
+
+                db.Siswas.Add( siswa );
+                db.SaveChanges();
+
+                MessageBox.Show("data berhasil ditambahkan");
+                loadData();
+            }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+
+            string columnName = dataGridView1.Columns[e.ColumnIndex].Name;
+
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+            if (columnName == "update")
+            {
+                idSiswa = Convert.ToInt32(row.Cells["Id"].Value);
+
+                textBoxNama.Text = row.Cells["Nama"].Value.ToString() ;
+                numericUpDownUmur.Value = Convert.ToInt32(row.Cells["Umur"].Value);
+                richTextBoxAlamat.Text = row.Cells["Alamat"].Value.ToString();
+                comboBoxJurusan.SelectedValue = row.Cells["jurusan"].Value.ToString();
+
+                buttonAdd.Text = "Update";
+            }
+            else if (columnName == "delete")
+            {
+                var result = MessageBox.Show("Hapus data ini??", "Konfirmasi", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    int idDelete = Convert.ToInt32(row.Cells["Id"].Value);
+                    var siswa = db.Siswas.Find(idDelete);
+
+                    if (siswa != null)
+                    {
+                        db.Siswas.Remove(siswa);
+                        db.SaveChanges();
+
+                        MessageBox.Show("Data sudah si hapus permanen");
+                    }
+                }
+
+                loadData();
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string key = textBoxSearch.Text;
+            if (key.Trim() == "" )
+            {
+                loadData();
+                return;
+            }
+
+            dataGridView1.DataSource = db.Siswas.Where(s => 
+                s.Nama.Contains(key) ||
+                s.Umur.ToString().Contains(key) ||
+                s.Alamat.Contains(key) ||
+                s.Jurusan.namaJurusan.Contains("key")
+
+            ).Select(s => new
+            {
+                s.Id,
+                s.Nama,
+                s.Umur,
+                s.Alamat,
+                Jurusan = s.Jurusan.namaJurusan
+            }).ToList();
+        }
+    }
+}
